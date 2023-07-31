1805bc7003a64937954daf4a235e864e450e57a6
2narim0823 <dlskfla0823@swu.ac.kr>
2023-07-31 18:40:27 +0900

    DB 값 수정

--- app/src/main/java/com/example/girokhanyang_0/database/MyDBHelper.kt
+++ app/src/main/java/com/example/girokhanyang_0/database/MyDBHelper.kt
@@ -13,13 +13,13 @@ class MyDBHelper(context: Context) : SQLiteOpenHelper(context, "myDB", null, 1) 
         // 일기 정보를 저장하는 diaryTBL(아이디, 내용, 기분(1~5), 작성시간)
         db!!.execSQL("CREATE TABLE diaryTBL ( id TEXT, contents TEXT, mood INT, writingTime TEXT, PRIMARY KEY(id, writingTime) );")
         db?.apply {
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기1', 3, '2023-08-01T10:00:56' );")
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기2', 2, '2023-07-30T21:10:41' );")
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기3', 1, '2023-07-30T17:35:56' );")
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기4', 5, '2023-07-29T13:00:35' );")
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기5', 3, '2023-07-24T08:16:56' );")
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기6', 1, '2023-07-08T16:08:56' );")
-            execSQL("INSERT INTO diaryTBL VALUES ( 'test000@naver.com', '작성된 일기7', 2, '2023-06-30T18:24:11' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기1', 3, '2023-08-01T10:00:56' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기2', 2, '2023-07-30T21:10:41' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기3', 1, '2023-07-30T17:35:56' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기4', 5, '2023-07-29T13:00:35' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기5', 3, '2023-07-24T08:16:56' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기6', 1, '2023-07-08T16:08:56' );")
+            execSQL("INSERT INTO diaryTBL VALUES ( 'test111@naver.com', '작성된 일기7', 2, '2023-06-30T18:24:11' );")
         }
     }
 
